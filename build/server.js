791b5d688bc0a2490c9ebe01600563877377868f
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2021-02-19 22:04:37 -0600

    Estado normal

--- build/server.js
+++ build/server.js
@@ -1,1 +1,45 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const helmet_1 = __importDefault(require("helmet"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const compression_1 = __importDefault(require("compression"));
+const cors_1 = __importDefault(require("cors"));
+const IndexRoutes_1 = __importDefault(require("./routes/IndexRoutes"));
+class Server {
+    constructor() {
+        this.app = express_1.default();
+        this.config();
+        this.routes();
+    }
+    config() {
+        const MONGO_URI = 'mongodb://localhost/restapit';
+        mongoose_1.default.set('useFindAndModify', true);
+        mongoose_1.default.connect(MONGO_URI || process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useCreateIndex: true
+        })
+            .then(db => console.log("DB is connected"));
+        this.app.set('port', process.env.PORT || 3000);
+        this.app.use(morgan_1.default('dev'));
+        this.app.use(express_1.default.json());
+        this.app.use(express_1.default.urlencoded({ extended: false }));
+        this.app.use(helmet_1.default());
+        this.app.use(compression_1.default());
+        this.app.use(cors_1.default());
+    }
+    routes() {
+        this.app.use(IndexRoutes_1.default);
+    }
+    start() {
+        this.app.listen(this.app.get('port'), () => {
+            console.log('Server on port', this.app.get('port'));
+        });
+    }
+}
+const server = new Server();
+server.start();
